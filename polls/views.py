20d4ca87c5f4178695b64e9ef3acedc3f522cbff
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2022-12-27 19:18:25 +0100

    Create views detail, results, and vote

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello! You're at the polls index.")
+	return HttpResponse("Bonjour! Ici l'index des sondages.")
+
+def detail(request, question_id):
+	return HttpResponse(f"Vous regardez la question n°{question_id}")
+
+def results(request, question_id):
+	return HttpResponse(f"Vous regardez les résultats de la question n°{question_id}")
+
+def vote(request, question_id):
+	return HttpResponse(f"Vous votez pour la question n°{question_id}")
