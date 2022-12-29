b605c2bab3bb4aabd85beeac4866409eaff9b5ea
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2022-12-29 09:39:30 +0100

    Raising a 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 
 def index(request):
@@ -10,7 +11,8 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse(f"Vous regardez la question n°{question_id}")
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	return HttpResponse(f"Vous regardez les résultats de la question n°{question_id}")
