80eef6be77cce2b3f8370a26dc76faeb5b5c6309
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2022-12-29 09:25:49 +0100

    Write views that actually do something
    Bonus: make index.html a complete HTML document.

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-	return HttpResponse("Bonjour! Ici l'index des sondages.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list': latest_question_list
+	}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse(f"Vous regardez la question n°{question_id}")
