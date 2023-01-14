c985c6df041b70d9fa1fbee86595888b907f4c56
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-01-14 16:44:00 +0100

    Write more comprhensive tests

--- polls/tests.py
+++ polls/tests.py
@@ -15,3 +15,21 @@ class QuestionModelTests(TestCase):
 		time = timezone.now() + datetime.timedelta(days=30)
 		future_question = Question(pub_date=time)
 		self.assertIs(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		"""
+		was_published_recently() returns False for questions whose pub_date
+		is older than 1 day.
+		"""
+		time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+		old_question = Question(pub_date=time)
+		self.assertIs(old_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_question(self):
+		"""
+		was_published_recently() returns True for questions whose pub_date
+		is within the last day.
+		"""
+		time = timezone.now() - datetime.timedelta(hours=23)
+		recently_published_question = Question(pub_date=time)
+		self.assertIs(recently_published_question.was_published_recently(), True)
